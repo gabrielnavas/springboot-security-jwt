2a4dc2e047c811be24f1ed7994da0ed924c7d72c
navas <gabrielnavas@email.com>
2023-10-03 00:34:20 -0300

    added the basic of the class JwtAuthenticationFilter

--- README.md
+++ README.md
@@ -122,3 +122,34 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findUserByUsername(String username);
 }
 ```
+
+### JwtAuthenticationFilter
+
+#### Toda vez que a API receber uma requisição, será chamado esse JwtAuthenticationFilter, podendo adicionar e remover Headeres por exemplo
+
+```java
+package com.navas.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
+```
---
+++ src/main/java/com/navas/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package com.navas.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String bearerName = "Bearer ";
+        final String username;
+        if (authHeader == null || !authHeader.startsWith(bearerName)) {
+            filterChain.doFilter(request, response);
+        }
+        jwt = authHeader.substring(bearerName.length());
+    }
+}
